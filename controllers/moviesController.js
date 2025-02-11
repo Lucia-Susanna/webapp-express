a9bb8504087adb7493a675ba12b3cb17b9863649
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-02-11 22:24:23 +0100

    feat: cors - img path

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -8,7 +8,14 @@ const index = (req, res) => {
   // connessione al database
   connection.query(sql, (err, results) => {
     if (err) return res.status(500).json({ error: 'query fallita' })
-    res.json(results)
+    const imagePath = 'http://localhost:3000/imgs/'
+    const movies = results.map(movie => {
+      return {
+        ...movie,
+        image: imagePath + movie.title.toLowerCase().replace(/\s+/g, '_') + '.jpg'
+      }
+    })
+    res.json(movies)
   })
 
 }
@@ -17,7 +24,7 @@ const show = (req, res) => {
   const id = req.params.id
   const sql = 'SELECT * FROM movies WHERE id=?'
   const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?'
-
+  const imagePath = 'http://localhost:3000/imgs/'
   connection.query(sql, [id], (err, results) => {
 
     if (err) return res.status(500).json({ error: 'query fallita' })
@@ -31,6 +38,7 @@ const show = (req, res) => {
       if (results.length === 0) return res.status(404).json({ error: 'film non trovato' })
 
       movie.reviews = reviewsResults
+      movie.image = imagePath + movie.title.toLowerCase().replace(/\s+/g, '_') + '.jpg'
       res.json(movie)
     })
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 require('dotenv').config()
+
+const cors = require('cors')
 const errorsHandler = require('./middleware/errorsHandler')
 const notfound = require('./middleware/notFound')
 
@@ -9,6 +11,10 @@ const moviesRouter = require('./routes/movies')
 
 app.use(express.static('public'))
 app.use(express.json())
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}))
 
 app.get('/', (req, res) => {
   res.send('server funzionante')
